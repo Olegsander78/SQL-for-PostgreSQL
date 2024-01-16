a284adfb45bfb299df36e31dd4498c051fbb875b
Oleg K <onkotov@gmail.com>
2024-01-16 09:58:54 +0500

    Update kotov_on_Создание таблиц и простые запросы.sql

--- kotov_on_Создание таблиц и простые запросы.sql
+++ kotov_on_Создание таблиц и простые запросы.sql
@@ -2,23 +2,23 @@
 --Схема kotov_on
 
  --   Какое количество платежей было совершено?
-select count(order_id) 
-from orders  
+SELECT COUNT(order_id) 
+FROM orders  
 --9465
 
  --   Какое количество товаров находится в категории «Игрушки»?
-select sum(p.remains) 
-from category c 
-join product p on c.category_id = p.category_id
-where c.category = 'Игрушки'
+SELECT SUM(p.remains) 
+FROM category c 
+JOIN product p ON c.category_id = p.category_id
+WHERE c.category = 'Игрушки'
 
-select sum(p.remains) 
-from category c, product p   
-where c.category_id = p.category_id and c.category = 'Игрушки'
+SELECT SUM(p.remains) 
+FROM category c, product p   
+WHERE c.category_id = p.category_id AND c.category = 'Игрушки'
 --998
 
  --   В какой категории находится больше всего товаров?
-SELECT c.category_id, c.category, sum(p.remains) as sum_cat
+SELECT c.category_id, c.category, SUM(p.remains) AS sum_cat
 FROM category c
 JOIN product p ON c.category_id = p.category_id
 GROUP BY c.category_id, c.category
@@ -27,11 +27,11 @@ LIMIT 1;
 --83	Музыка	64000.00
 
  --   Сколько «Черепах» купила Williams Linda?
-select c.last_name, c.first_name, p.product , sum(opl.amount)
-from customer c
-join orders o on c.customer_id = o.customer_id
-join order_product_list opl on o.order_id = opl.order_id 
-join product p on opl.product_id = p.product_id 
-where (c.last_name = 'Williams' and c.first_name = 'Linda') and p.product = 'Черепаха'
-group by c.last_name, c.first_name, p.product
+SELECT c.last_name, c.first_name, p.product , SUM(opl.amount)
+FROM customer c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_product_list opl ON o.order_id = opl.order_id 
+JOIN product p ON opl.product_id = p.product_id 
+WHERE (c.last_name = 'Williams' AND c.first_name = 'Linda') AND p.product = 'Черепаха'
+GROUP BY c.last_name, c.first_name, p.product
 --Williams	Linda	Черепаха	3.00
